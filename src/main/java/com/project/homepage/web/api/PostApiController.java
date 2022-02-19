52708bcfb0f0d99b9df67541cf58e60334e37986
gyuyeon <gks951020@gmail.com>
2022-02-19 18:05:40 +0900

    Api Controller 를 리액트와 연동

--- src/main/java/com/project/homepage/web/api/PostApiController.java
+++ src/main/java/com/project/homepage/web/api/PostApiController.java
@@ -4,7 +4,14 @@ import com.project.homepage.service.PostService;
 import com.project.homepage.web.dto.post.PostResponseDto;
 import com.project.homepage.web.dto.post.PostSaveRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,13 +22,20 @@ public class PostApiController {
 
     private final PostService postService;
 
+    @Value("${origin}")
+    private String origin;
+
     @GetMapping
-    public List<PostResponseDto> getAllPostList() {
+    public ResponseEntity<List<PostResponseDto>> getAllPostList() {
         List<PostResponseDto> postDtoList = postService.findAllPost().stream()
                 .map(PostResponseDto::new)
                 .collect(Collectors.toList());
 
-        return postDtoList;
+        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+        headers.setAccessControlAllowOrigin(origin);
+
+        return new ResponseEntity<>(postDtoList, headers, HttpStatus.OK);
     }
 
     @GetMapping("/{postId}")
